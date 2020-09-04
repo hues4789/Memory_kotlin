ecb3ccefa1cdcba4195a0ab463495324b0327294
hues4789 <yosikawashuhei0225@gmail.com>
2020-09-04 21:19:05 +0900

    today画面、task_create画面作成

--- app/src/main/java/com/memory/memory_kotlin/task_create.kt
+++ app/src/main/java/com/memory/memory_kotlin/task_create.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class task_create : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_task_create)
 
         btToday.setOnClickListener{onCommonButtonClick(it)}
         btCreate.setOnClickListener{onCommonButtonClick(it)}
