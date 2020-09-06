716ba263db372f198d4c04897550c814b63840a0
hues4789 <yosikawashuhei0225@gmail.com>
2020-09-06 11:33:16 +0900

    学習リスト表示画面-DB処理

---
+++ app/src/main/java/com/memory/memory_kotlin/Adapter.kt
@@ -0,0 +1,40 @@
+package com.memory.memory_kotlin
+
+import android.content.Context
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmRecyclerViewAdapter
+
+class Adapter (
+    private val context: Context,
+    //data
+    private val List: OrderedRealmCollection<Tasks>?,
+    private val autoUpdate: Boolean
+):RealmRecyclerViewAdapter<Tasks,Adapter.ViewHolder>(List,autoUpdate){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val task: TextView = view.findViewById(R.id.task)
+        val date: TextView = view.findViewById(R.id.create_dt)
+    }
+
+    //リストの取得件数
+    override fun getItemCount(): Int {
+        return List?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false)
+        return ViewHolder(view)
+    }
+    //データを取り出して表示する
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val tasks = List?.get(position)
+        holder.task.text = tasks?.learnContext
+        holder.date.text = DateFormat.format("yyyy/MM/dd",tasks?.created_at)
+    }
+}
--- app/src/main/java/com/memory/memory_kotlin/MainActivity.kt
+++ app/src/main/java/com/memory/memory_kotlin/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         Realm.init(this)
         mRealm = Realm.getDefaultInstance()
+
+        val read = readList()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        //RecyclerViewのAdapter、layouｔなどの設定
+        val adapter = Adapter(this, read, true)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
 
         btToday.setOnClickListener{onCommonButtonClick(it)}
         btCreate.setOnClickListener{onCommonButtonClick(it)}
@@ -37,10 +50,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //override fun onDestroy() {
-      //  super.onDestroy()
-    //    mRealm.close()
-    //}
+    fun readList(): RealmResults<Tasks> {
+        return mRealm.where(Tasks::class.java).findAll()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRealm.close()
+    }
 
 
 }
